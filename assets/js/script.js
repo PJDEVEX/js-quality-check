ed24827100b070b61a590996253466c87f2bd121
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-03-14 22:08:27 +0000

    Practice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Define constants for API_KEY, API_URL and document
 const API_KEY = "8jxsbfzhZX1JG4HIQW7-vZH9Qs0";
-const API_URL = "http://ci-jshint.hrtokuapp.com/api";
-const resultsModal = new bootstrap.Model(document.getElementById("resultsModal"));
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 // it is a good practice to pass "e" to handler function
 /**
@@ -9,8 +9,8 @@ const resultsModal = new bootstrap.Model(document.getElementById("resultsModal")
  */
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) {
 
-async function getStatus (e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
 
     const response = await fetch(queryString);
@@ -18,6 +18,20 @@ async function getStatus (e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+}
+
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div>Your API key is valid until: </div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
 }
